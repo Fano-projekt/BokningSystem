90c474ce88cfbc88bc2d2bb37f27f51345bac015
Fano-projekt <afnaankamal2000@gmail.com>
2025-03-30 01:03:59 +0100

    Add files via upload

---
+++ wwwroot/js/Home.js
@@ -0,0 +1,10 @@
+﻿
+window.onscroll = function () {
+    let button = document.getElementById("back-to-top");
+    button.style.display = (window.scrollY > 300) ? "block" : "none";
+};
+
+// Scrolla 
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
